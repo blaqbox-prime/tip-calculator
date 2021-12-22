89d11de7c14bbdc25178d244b027e44fe362ebad
blaqbox-prime <karabosambo@outlook.com>
2021-12-22 11:57:23 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <h1 className="App__title">
+          SPLI<br/>TTER
+        </h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/TipSelector.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function TipSelector() {
+    return (
+        <div className='TipSelector'>
+            
+        </div>
+    )
+}
+
+export default TipSelector
